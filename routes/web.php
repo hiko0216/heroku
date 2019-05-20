92b0b00beb8dc25d02c7f60aa889e2016f7e3252
彦坂真一 <hikosakashinichi@mba.local>
2019-05-20 09:59:27 +0530

    create route

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,51 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[
+    'uses'=>'HomeController@list',
+    'as'=>'top'
+]);
+Route::get('/profile',[
+    'uses'=>'PostsController@create',
+    'as'=>'post.create'
+]);
+// Route::get('/redirect','SocialFacebookController@redirect');
+// Route::get('/auth/facebook/callback','SocialFacebookController@callback');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/profile/index',[
+    'uses'=>'ProfileController@index',
+    'as'=>'profile.index'
+]);
+Route::post('/profile/update',[
+    'uses'=>'ProfileController@update',
+    'as'=>'profile.update'
+]);
+Route::post('/post/store',[
+    'uses'=>'PostsController@store',
+    'as'=>'post.store'
+]);
+Route::get('/post/show',[
+    'uses'=>'PostsController@show',
+    'as'=>'post.show'
+]);
+Route::get('/post/single/{post}',[
+    'uses'=>'HomeController@single',
+    'as'=>'single.show'
+]);
+Route::get('/post/edit/{post}',[
+    'uses'=>'PostsController@edit',
+    'as'=>'post.edit'
+]);
+Route::post('/post/update/{post}',[
+    'uses'=>'PostsController@update',
+    'as'=>'post.update'
+]);
+
+Route::any('/search', 'HomeController@search');
+
+Route::get('/detail',function(){
+    return view('detail');
+});
