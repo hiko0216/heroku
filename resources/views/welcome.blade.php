0e2e451e05cad56679295f867f4389025a1fec66
彦坂真一 <hikosakashinichi@mba.local>
2019-05-20 09:34:32 +0530

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HOME ABROAD
                 </div>
 
                 <div class="links">
