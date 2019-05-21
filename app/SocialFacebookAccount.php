54f543e7f5aa88f703a5b5eefa47771f3ee675b8
彦坂真一 <hikosakashinichi@mba.local>
2019-05-21 16:52:26 +0530

    facebook table update

--- app/SocialFacebookAccount.php
+++ app/SocialFacebookAccount.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialFacebookAccount extends Model
 {
-    protected $fillable=['user_id','provider_user_id','provider'];
+    protected $fillable=['id','user_id','provider_user_id','provider'];
 
     public function user(){
         return $this->belongsTo(User::class);
