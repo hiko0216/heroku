8f63eb5e8b7354cede3aec1145fb39ffd24d1385
彦坂真一 <hikosakashinichi@mba.local>
2019-05-21 16:59:18 +0530

    facebook table update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
                     
                                     <div class="card-body">
                                         <form method="POST" action="{{ route('register') }}">
-                                            @csrf
+                                            {{csrf_field()}}
                     
                                             <div class="form-group row">
                                                 <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
@@ -91,7 +91,7 @@
                         
                                         <div class="card-body">
                                             <form method="POST" action="{{ route('login') }}">
-                                                @csrf
+                                                {{csrf_field()}}
                         
                                                 <div class="form-group row">
                                                     <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
