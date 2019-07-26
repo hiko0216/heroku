121fc37e2d6dec612cb4d6208c8d804ce8a08dc1
彦坂真一 <hikosakashinichi@mba.local>
2019-07-26 18:52:53 +0700

    change css

--- resources/views/layouts/payapp.blade.php
+++ resources/views/layouts/payapp.blade.php
@@ -12,6 +12,9 @@
 
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,8 +22,13 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <style>
+    .post{
+        margin:0!important;
+    }
     .navbar{
         height: 130px;
         padding-top: 0;
@@ -33,6 +41,23 @@
    }
     .dropdown-toggle:after{
     display: none;
+  }
+  @media screen and (max-width:750px){
+      select{
+        border: 1px solid #ccc;
+      }
+      .detail{
+          margin-top: 30px;
+      }
+      .formtitle{
+          margin-bottom: 5px!important;
+          font-size:27px;
+          font-weight: 600;
+      }
+      .dropdown-menu{
+          background: none;
+          text-align: right;
+      }
   }
 </style>
 <body>
